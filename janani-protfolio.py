78f0f90a269be8f97480eff96102fd1d4ae7c702
akshayaa-2024-aia <akshayaasudhaharan13@gmail.com>
2024-11-04 19:21:39 +0530

    Created protfolio

---
+++ janani-protfolio.py
@@ -0,0 +1,24 @@
+import streamli as st
+
+st.title("Protfolio")
+
+st.header("About Me")
+st.write("""
+I'm janani, a 1st-year B.Tech student in Artificial Intelligence and Data Science.
+""")
+
+st.header("Interests")
+st.write("""
+* solving mathematical problems
+* Research and learning
+""")
+
+st.header("Education")
+st.write("""
+* Ist year B.Tech in Artificial Intelligence and Data Science, 
+  College Name:KGisl institue of technology 
+  Academic Year:2024-2028
+""")
+
+st.header("Contact:")
+st.write("jananithangaraj11@gmail.com")
